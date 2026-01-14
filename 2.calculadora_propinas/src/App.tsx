b708156ee914c810a1acf42ba89b7545a59c0636
paulo <paulojosueb58@gmail.com>
2026-01-13 21:52:45 -0600

    siguiendo con el curso

--- 2.calculadora_propinas/src/App.tsx
+++ 2.calculadora_propinas/src/App.tsx
@@ -1,4 +1,4 @@
-import { MenuItem } from "./components/MenuItem"
+import {  MenuItemC } from "./components/MenuItemC"
 import { menuItems } from "./data/db"
 
 
@@ -17,15 +17,22 @@ console.log(menuItems)
 
 
      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
-      <div>
-        <h2>Menu</h2>
+      <div className="p-5">
+        <h2 className="text-4xl font-black">Menu</h2>
 
-        {menuItems.map(item=>(
-          <MenuItem
+
+        <div className="space-y-3 mt-10">
+
+       
+        {menuItems.map(item => (
+          <MenuItemC
           key={item.id}
+          item ={item}
           />
         ))}
+
       </div>
+       </div>
 
       <div>
         <h2>Consumo</h2>
