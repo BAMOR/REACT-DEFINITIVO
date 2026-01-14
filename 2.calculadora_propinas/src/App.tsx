18f5e9e9b314b09b304e31d4015673f5ba97de5b
paulo <paulojosueb58@gmail.com>
2026-01-14 16:50:55 -0600

    continuando

--- 2.calculadora_propinas/src/App.tsx
+++ 2.calculadora_propinas/src/App.tsx
@@ -1,13 +1,18 @@
 import {  MenuItemC } from "./components/MenuItemC"
+import { OrderContents } from "./components/OrderContents"
 import { menuItems } from "./data/db"
 
+
+import {useOrder} from "./hooks/useOrder"
 
 
 
 
 function App() {
 
-console.log(menuItems)
+const {addItem, order} = useOrder()
+
+
   return (
     <>
     
@@ -28,14 +33,17 @@ console.log(menuItems)
           <MenuItemC
           key={item.id}
           item ={item}
+          addItem = {addItem}
           />
         ))}
 
       </div>
        </div>
 
-      <div>
-        <h2>Consumo</h2>
+      <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+        <OrderContents
+        order = {order}
+        />
       </div>
 
      </main>
