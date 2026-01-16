e43e9ee935b0e0b246af0ccd14e489252d5142c7
paulo <paulojosueb58@gmail.com>
2026-01-15 22:48:12 -0600

    iniciando un proyecto nuevo

---
+++ EJERCICIOS/2.tienda-personal/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-4xl text-black uppercase">hola mundo</h1>
+    </>
+  )
+}
+
+export default App
---
+++ EJERCICIOS/2.tienda-personal/src/data/products.ts
@@ -0,0 +1,31 @@
+
+export const initialProducts = [
+  {
+    id: 1,
+    name: "Auriculares Bluetooth",
+    price: 49.99,
+    category: "tecnología",
+    isFavorite: true
+  },
+  {
+    id: 2,
+    name: "Camiseta de algodón",
+    price: 19.99,
+    category: "ropa",
+    isFavorite: false
+  },
+  {
+    id: 3,
+    name: "Libro: Aprendiendo React",
+    price: 24.5,
+    category: "libros",
+    isFavorite: true
+  },
+  {
+    id: 4,
+    name: "Mochila urbana",
+    price: 35.0,
+    category: "accesorios",
+    isFavorite: false
+  }
+];
